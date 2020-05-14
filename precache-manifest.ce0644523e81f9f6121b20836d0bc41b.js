ff2d08877f8daef346efc46d13f036c89679d48c
brian <brian.oliveira100@gmail.com>
2020-05-14 13:49:31 -0400

    Updates

---
+++ precache-manifest.ce0644523e81f9f6121b20836d0bc41b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c64ec290c549f97e26f4cae060a2601",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63caedc5c17a18fc7fe5",
+    "url": "/static/css/main.123a54ec.chunk.css"
+  },
+  {
+    "revision": "1094a0366719518a98a3",
+    "url": "/static/js/2.041ac762.chunk.js"
+  },
+  {
+    "revision": "dcd1d80362aef046a5450470cca1218a",
+    "url": "/static/js/2.041ac762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63caedc5c17a18fc7fe5",
+    "url": "/static/js/main.d9afc876.chunk.js"
+  },
+  {
+    "revision": "5911a9a3e605901dce30",
+    "url": "/static/js/runtime-main.84697ae4.js"
+  }
+]);
